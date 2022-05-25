17f76103c33848a5aca167ac36609285e286c0c8
Tran Vinh Luan <tranvinhluanluuxa@gmail.com>
2022-05-25 18:29:04 +0700

    first up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+import './pages/CreateOrder';
+
+import CreateOrder from './pages/CreateOrder';
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path="/createorder" element={<CreateOrder />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
