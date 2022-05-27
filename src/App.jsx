85922b6ba35167d60c3e001f1ba2a6e50a46fec3
Mrcuong1996 <vanlytuongtuluv@gmail.com>
2022-05-27 17:59:29 +0700

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,40 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
 import "./App.css";
-import Login from "./Pages/Admin/Login/login";
-import Home from "./Pages/Admin/Home/home";
-import QLnhanvien from "./Pages/Admin/NhanVien/Nhanvien";
-import Xacnhan from "./Pages/Admin/Donhang/Xacnhan";
-import Tongket from "./Pages/Admin/Donhang/Tongket";
-import Hoanthanh from "./Pages/Admin/Donhang/Hoanthanh";
-import Danggiao from "./Pages/Admin/Donhang/Danggiao";
-import Chinhsua from "./Pages/Admin/Sanpham/Chinhsua";
-import Khohang from "./Pages/Admin/Sanpham/Khohang";
-import Spmoi from "./Pages/Admin/Sanpham/Spmoi";
-import Trenke from "./Pages/Admin/Sanpham/Trenke";
-import Chinhanh from "./Pages/Admin/Thongtin/Chinhanh";
-import About from "./Pages/Admin/Thongtin/About";
-import Header from "./Header/Header";
-import UserLogin from "./User/UserLogin";
-import UserSingIn from "./User/UserSingIn";
-import UserPase from "./User/UserPase";
-import { useState, useEffect } from "react";
-import ContextProvider from "./Conter/ContextProvider";
+import "antd/dist/antd.css";
+
+import Header from "./compunentes/header/Header";
+import Footer from "./compunentes/footer/Footer";
+import Slider from "./compunentes/slider/Slider";
+import Home from "./compunentes/home/Home";
+import SeeMore from "./compunentes/home/homePage/SeeMore";
+import Advertisement from "./advertisement/Advertisement";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Cards from "./compunentes/home/homePage/Cards";
 
 function App() {
-  const [user, setUser] = useState([
-    {
-      email: "hoang@gmail.com",
-      password: "123456789",
-    },
-    {
-      email: "hoang@gmail.com",
-      password: "123456789",
-    },
-    {
-      email: "hoang@gmail.com",
-      password: "123456789",
-    },
-  ]);
-    // tín hiệu để render
-  //   const [stoge, setStoge]=useState(0)
-  //   function setStoge1(){
-  //     setStoge(stoge+1)
-  //   }
-  //   // Gọi data về
-  //   useEffect(() => {
-  //     axios.get('')
-  //     .then((data)=>{
-  //       localStorage.setItem('user',JSON.stringify(data))
-  //       console.log(data)
-  //     })
-  //     .catch((arr)=>{
-  //       console.log(arr)
-  //     })
-  //   }, [stoge]);
-  // const [user, setUser]=useState([JSON.parse(localStorage.getItem('user'))])
+  const [Categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3150/admin/categories")
+      .then(function (res) {
+        setCategories(res.data);
+        console.log(res.data);
+      })
+      .catch(function (err) {
+        console.log(99, err);
+      });
+  }, []);
+
   return (
-    <div>
-      <BrowserRouter>
-        <ContextProvider testContext={{ user: user, setUser: setUser }}>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/admin/login" element={<Login />} />
-            <Route path="/admin/home" element={<Home />} />
-            <Route path="/admin/qlnhanvien" element={<QLnhanvien />} />
-            <Route path="/admin/Xacnhan" element={<Xacnhan />} />
-            <Route path="/admin/Tongket" element={<Tongket />} />
-            <Route path="/admin/Hoanthanh" element={<Hoanthanh />} />
-            <Route path="/admin/Danggiao" element={<Danggiao />} />
-            <Route path="/admin/Chinhsua" element={<Chinhsua />} />
-            <Route path="/admin/Khohang" element={<Khohang />} />
-            <Route path="/admin/Spmoi" element={<Spmoi />} />
-            <Route path="/admin/Trenke" element={<Trenke />} />
-            <Route path="/admin/Chinhanh" element={<Chinhanh />} />
-            <Route path="/admin/About" element={<About />} />
-            <Route path="/User/UserLogin" element={<UserLogin></UserLogin>} />
-            <Route path="/User/UserSingIn" element={<UserSingIn></UserSingIn>} />
-            <Route path="/User/UserPase" element={<UserPase></UserPase>} />
-            {/* <Route path="/Home/Home" element={<Home></Home>} /> */}
-          </Routes>
-        </ContextProvider>
-      </BrowserRouter>
+    <div className="App">
+      <Header />
+      <Slider Categories={Categories} />
+      <Home />
+      <SeeMore />
+      <Advertisement />
+      <Footer />
     </div>
   );
 }
---
+++ src/compunentes/slider/dataSlider.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+
+const dataSlider = [
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+  {
+    id: uuidv4(),
+    title: "Advertisement",
+    subTitle: "Lorem",
+  },
+];
+
+export default dataSlider;
