4f6055641bca74b3e8253e0548e7ababf2f7ed02
VinhPhan301 <vinhphan301@gmail.com>
2022-06-01 09:03:04 +0700

    106

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Login from "./Pages/Admin/Login/login";
+// import "./App.css";
 import Home from "./Pages/Admin/Home/home";
-import QLnhanvien from "./Pages/Admin/NhanVien/Nhanvien";
 import Xacnhan from "./Pages/Admin/Donhang/Xacnhan";
 import Tongket from "./Pages/Admin/Donhang/Tongket";
 import Hoanthanh from "./Pages/Admin/Donhang/Hoanthanh";
@@ -13,24 +11,80 @@ import Spmoi from "./Pages/Admin/Sanpham/Spmoi";
 import Trenke from "./Pages/Admin/Sanpham/Trenke";
 import Chinhanh from "./Pages/Admin/Thongtin/Chinhanh";
 import About from "./Pages/Admin/Thongtin/About";
+import Nhanvien from "./Pages/Admin/NhanVien/Nhanvien";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setdata] = useState([]);
+  const [showdata, setshowdata] = useState([]);
+  const [brand, setbrand] = useState([]);
+  const [sign, setsign] = useState(0);
+  const [model, setmodel] = useState([]);
+  const [listdt, setlistdt] = useState([]);
+  function changesign() {
+    setsign(sign + 1);
+  }
+  useEffect(() => {
+    axios
+      .get("http://localhost:3150/admin/productcode/list")
+      .then(function (response) {
+        setdata(response.data);
+        setshowdata(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3150/admin/categories")
+      .then(function (response) {
+        setbrand(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/admin/login" element={<Login />} />
           <Route path="/admin/home" element={<Home />} />
-          <Route path="/admin/qlnhanvien" element={<QLnhanvien />} />
+          <Route path="/admin/qlnhanvien" element={<Nhanvien />} />
           <Route path="/admin/Xacnhan" element={<Xacnhan />} />
           <Route path="/admin/Tongket" element={<Tongket />} />
           <Route path="/admin/Hoanthanh" element={<Hoanthanh />} />
           <Route path="/admin/Danggiao" element={<Danggiao />} />
-          <Route path="/admin/Chinhsua" element={<Chinhsua />} />
+          <Route
+            path="/admin/Chinhsua"
+            element={
+              <Chinhsua
+                model={model}
+                setmodel={setmodel}
+                changesign={changesign}
+                listdt={listdt}
+                setlistdt={setlistdt}
+              />
+            }
+          />
           <Route path="/admin/Khohang" element={<Khohang />} />
           <Route path="/admin/Spmoi" element={<Spmoi />} />
-          <Route path="/admin/Trenke" element={<Trenke />} />
+          <Route
+            path="/admin/Trenke"
+            element={
+              <Trenke
+                data={data}
+                brand={brand}
+                sign={sign}
+                changesign={changesign}
+                setdata={setdata}
+                showdata={showdata}
+                setshowdata={setshowdata}
+              />
+            }
+          />
           <Route path="/admin/Chinhanh" element={<Chinhanh />} />
           <Route path="/admin/About" element={<About />} />
         </Routes>
--- src/pages/Admin/Sanpham/Trenke.jsx
+++ src/pages/Admin/Sanpham/Trenke.jsx
@@ -1,13 +1,101 @@
-import React from "react";
 import Header from "../../../Components/Header/header";
+import "./product.css";
+import { render } from "@testing-library/react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Pagination } from "antd";
+import axios from "axios";
 
-function Trenke() {
+function Trenke(props) {
+  var abcarr;
+  function choosebrand(id) {
+    // document.querySelector(".newproduct").style.height = "100vh";
+    let listBrand = document.querySelectorAll(".brand");
+    for (let i = 0; i < listBrand.length; i++) {
+      listBrand[i].setAttribute("style", "");
+    }
+    document.querySelector(`[value="${id}"]`).style.background = "black";
+    document.querySelector(`[value="${id}"]`).style.color = "white";
+    abcarr = [];
+    props.data.map(function (value, index) {
+      if (value.idCategories[0] === id) {
+        abcarr.push(value);
+        props.setshowdata(abcarr);
+      }
+    });
+  }
+  function showall() {
+    let listBrand = document.querySelectorAll(".brand");
+    for (let i = 0; i < listBrand.length; i++) {
+      listBrand[i].setAttribute("style", "");
+    }
+    document.querySelector("#tatca").style.background = "black";
+    document.querySelector("#tatca").style.color = "white";
+    props.setshowdata(props.data);
+  }
   return (
-    <Header>
-      <div>
-        <h1>Tren ke</h1>
+    <div>
+      <Header></Header>
+      <div className="newproduct">
+        <h1 className="sptk">Sản phẩm trên kệ</h1>
+        <div className="allbrand">
+          <div className="brand tatca123" id="tatca">
+            <p id="brand" className="tatca" onClick={showall}>
+              Tất cả
+            </p>
+          </div>
+          {props.brand.map(function (value, index) {
+            return (
+              <div
+                className="brand"
+                value={value._id}
+                key={index}
+                onClick={() => choosebrand(value._id)}
+              >
+                <p id="brand">{value.categoriesName}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="boxtable">
+          <table>
+            <thead>
+              <tr>
+                <th>STT</th>
+                <th>Tên sản phẩm</th>
+                <th>Hình ảnh</th>
+                <th>Loại sản phẩm</th>
+                <th>Thông tin sản phẩm</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.showdata.map(function (value, index) {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{value.productName}</td>
+                    <td>
+                      <img
+                        src={"http://localhost:3150" + value.thumNail}
+                        alt=""
+                      />
+                    </td>
+                    <td>{value.productType}</td>
+                    <td>
+                      {value.performanceProduct} <br />
+                      {value.cameraProduct} <br />
+                      {value.specialFeatures} <br />
+                      {value.design} <br />
+                      {value.panel}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </Header>
+    </div>
   );
 }
 
