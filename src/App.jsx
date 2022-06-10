de710116b0e0e1a56fccdd68222d8c52641b03ea
Tran Vinh Luan <tranvinhluanluuxa@gmail.com>
2022-06-10 10:08:10 +0700

    save

--- src/App.jsx
+++ src/App.jsx
@@ -19,71 +19,107 @@ import UserPase from "./User/UserPase";
 import ContextProvider from "./Conter/ContextProvider";
 import Home1 from "./compunentes/home/Home";
 import CreateOrder from "./Pages/CreateOrder";
-import './App.css';
-import ProductChild from './Pages/ProductChild';
-import FilterProduct from './Pages/FilterProduct'
-import 'antd/dist/antd.css'//ở trong nodemodum
+import "./App.css";
+import ProductChild from "./Pages/ProductChild";
+import FilterProduct from "./Pages/FilterProduct";
+import "antd/dist/antd.css"; //ở trong nodemodum
 // ==============================================
 // ===============================================
 //data
-import productList from './data-tinh/dataold';
-import listProductCode from './data-tinh/dataNewMix'
-const App = props => {
-  const [count1, setCount1] = useState(0)
-  const [dataFilter, setDataFilter] = useState([])
+import productList from "./data-tinh/dataold";
+import listProductCode from "./data-tinh/dataNewMix";
+const App = (props) => {
+  const [count1, setCount1] = useState(0);
+  const [dataFilter, setDataFilter] = useState([]);
   const filterProduct = {
-    brand: ['Iphone', 'Samsung', 'Oppo', 'Vivo', 'Xiaomi', 'Realmi', 'Nokia', 'Itel', 'masstel'],
-    price: ['dưới 2tr', 'từ 2- 4tr', 'từ 4-7tr', 'từ 7-13tr', 'từ 13-20tr', 'trên 20tr'],
-    productType: ['android', 'Iphone(iOS)', 'Điện thoại phổ thông'],
-    performanceProduct: ['chơi game/cấu hình cao', 'Pin khủng trên 5000 mAh', 'Sạc pin nhanh'],
-    ram: ['2GB', '3GB', '4GB', '6GB', '8GB', '12GB'],
-    rom: ['8GB', '16GB', '32GB', '64GB', '128GB', '256GB'],
-    cameraProduct: ['chụp cận cảnh(macro)', 'chụp góc rộng', 'chụp xóa phông', 'chụp zoom xa'],
-    specialFeatures: ['Hỗ trợ 5g', 'Bảo mật khuôn mặt', 'bảo mật vân tay', 'Sạc không dây', 'Kháng nước , bụi'],
-    design: ['Tràn viền', 'Mỏng nhẹ', 'Mặt lưng kính'],
-    panel: ['nhỏ gọn dễ cầm', 'Từ 6inch trở lên', 'Màn hình gập']
-  }
+    brand: [
+      "Iphone",
+      "Samsung",
+      "Oppo",
+      "Vivo",
+      "Xiaomi",
+      "Realmi",
+      "Nokia",
+      "Itel",
+      "masstel",
+    ],
+    price: [
+      "dưới 2tr",
+      "từ 2- 4tr",
+      "từ 4-7tr",
+      "từ 7-13tr",
+      "từ 13-20tr",
+      "trên 20tr",
+    ],
+    productType: ["android", "Iphone(iOS)", "Điện thoại phổ thông"],
+    performanceProduct: [
+      "chơi game/cấu hình cao",
+      "Pin khủng trên 5000 mAh",
+      "Sạc pin nhanh",
+    ],
+    ram: ["2GB", "3GB", "4GB", "6GB", "8GB", "12GB"],
+    rom: ["8GB", "16GB", "32GB", "64GB", "128GB", "256GB"],
+    cameraProduct: [
+      "chụp cận cảnh(macro)",
+      "chụp góc rộng",
+      "chụp xóa phông",
+      "chụp zoom xa",
+    ],
+    specialFeatures: [
+      "Hỗ trợ 5g",
+      "Bảo mật khuôn mặt",
+      "bảo mật vân tay",
+      "Sạc không dây",
+      "Kháng nước , bụi",
+    ],
+    design: ["Tràn viền", "Mỏng nhẹ", "Mặt lưng kính"],
+    panel: ["nhỏ gọn dễ cầm", "Từ 6inch trở lên", "Màn hình gập"],
+  };
   // color: Xanh , Đỏ , Tím , Hồng, Đen
- let productCode = []
+  let productCode = [];
   // const [ProductList, setProductList] = useState(listProductCode.listProductCode)
-  const [ProductList, setProductList] = useState(productList)
-   //---------------------------
+  const [ProductList, setProductList] = useState(productList);
+  //---------------------------
   // axious project sellMobilePhone
   useEffect(() => {
-    axios.get('http://localhost:3150/user/fillter?idCategories=628c8b29e8654d960a5c8983')
+    axios
+      .get(
+        "http://localhost:3150/user/fillter?idCategories=628c8b29e8654d960a5c8983"
+      )
       .then(function (res) {
         // setDataDuy(res.data.product)
         // setShow(res.data.product.slice(0, 2))
-        console.log(45, res.data.listData)
-        const ListData= res.data.listProductCode.map((val)=>{
-          val.storage =Math.floor(Math.random() * 100);
-          val.ram=val.ramRange[0];
-          val.rom=val.romRange[0];
-          return val
-        })
-        console.log(4556, ListData)
-        setProductList([...ListData])
+        console.log(45, res.data.listData);
+        const ListData = res.data.listProductCode.map((val) => {
+          val.storage = Math.floor(Math.random() * 100);
+          val.ram = val.ramRange[0];
+          val.rom = val.romRange[0];
+          return val;
+        });
+        console.log(4556, ListData);
+        setProductList([...ListData]);
       })
       .catch((err) => {
-        console.log(err)
-      })
+        console.log(err);
+      });
   }, []);
-  const [dataProduct, setDataProduce] = useState(productCode)
-  const [count, setCount] = useState(0)
-  const [filter, setFilterProduct] = useState(filterProduct)
+  const [dataProduct, setDataProduce] = useState(productCode);
+  const [count, setCount] = useState(0);
+  const [filter, setFilterProduct] = useState(filterProduct);
   function changeStateProduct(newData, i) {
     dataProduct[i].ProductList = newData;
-    setDataProduce(dataProduct)
+    setDataProduce(dataProduct);
   }
   function changeFilterData(dataFilter) {
-    setDataFilter(dataFilter)
+    setDataFilter(dataFilter);
   }
   // -------------------function biến đổi tiếng việt có dấu thành không dấu.
   function RemoveAccents(str) {
     var AccentsMap = [
       "aàảãáạăằẳẵắặâầẩẫấậ",
       "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
-      "dđ", "DĐ",
+      "dđ",
+      "DĐ",
       "eèẻẽéẹêềểễếệ",
       "EÈẺẼÉẸÊỀỂỄẾỆ",
       "iìỉĩíị",
@@ -93,16 +129,15 @@ const App = props => {
       "uùủũúụưừửữứự",
       "UÙỦŨÚỤƯỪỬỮỨỰ",
       "yỳỷỹýỵ",
-      "YỲỶỸÝỴ"
+      "YỲỶỸÝỴ",
     ];
     for (var i = 0; i < AccentsMap.length; i++) {
-      var re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
+      var re = new RegExp("[" + AccentsMap[i].substr(1) + "]", "g");
       var char = AccentsMap[i][0];
       str = str.replace(re, char);
     }
     return str;
   }
-
 
   const [name, setName] = useState("");
   function changedata(newdata) {
@@ -121,44 +156,44 @@ const App = props => {
   }
   const [Categories, setCategories] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("/admin/categories")
-      .then(function (res) {
-        console.log(44, res.data);
-        setCategories(res.data);
-        // console.log(res.data);
-      })
-      .catch(function (err) {
-        console.log(99, err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/admin/categories")
+  //     .then(function (res) {
+  //       console.log(44, res.data);
+  //       setCategories(res.data);
+  //       // console.log(res.data);
+  //     })
+  //     .catch(function (err) {
+  //       console.log(99, err);
+  //     });
+  // }, []);
 
   function changesign() {
     setsign(sign + 1);
   }
-  useEffect(() => {
-    axios
-      .get("/admin/productcode/list")
-      .then(function (response) {
-        setdata(response.data);
-        setshowdata(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/admin/productcode/list")
+  //     .then(function (response) {
+  //       setdata(response.data);
+  //       setshowdata(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }, []);
 
-  useEffect(() => {
-    axios
-      .get("/admin/categories")
-      .then(function (response) {
-        setbrand(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("/admin/categories")
+  //     .then(function (response) {
+  //       setbrand(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }, []);
 
   function changesign() {
     setsign(sign + 1);
@@ -170,59 +205,202 @@ const App = props => {
           <Routes>
             {dataFilter.map((val, i) => {
               return (
-                <Route path={`/product/filter/${RemoveAccents(val.productName).split(' ').join('')}`} element={<ProductChild dataFilter={dataFilter} chimuc={i} dataval={ProductList}    changeStateProduct={changeStateProduct}  />} />
-              )
+                <Route
+                  path={`/product/filter/${RemoveAccents(val.productName)
+                    .split(" ")
+                    .join("")}`}
+                  element={
+                    <ProductChild
+                      dataFilter={dataFilter}
+                      chimuc={i}
+                      dataval={ProductList}
+                      changeStateProduct={changeStateProduct}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter brand */}
             {filterProduct.brand.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='brand' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="brand"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter giá */}
             {filterProduct.price.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='price' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="price"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter loại điện thoại */}
             {filterProduct.productType.map((val, i) => {
-
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='typePhone' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="typePhone"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter hiệu năng và pin */}
             {filterProduct.performanceProduct.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='performanceProduct' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="performanceProduct"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter ram */}
             {filterProduct.ram.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='ram' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="ram"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter bộ nhớ trong */}
             {filterProduct.rom.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='rom' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="rom"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter bộ nhớ cameraProduct */}
             {filterProduct.cameraProduct.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='cameraProduct' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="cameraProduct"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter special feature */}
             {filterProduct.specialFeatures.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='special' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="special"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter design  */}
             {filterProduct.design.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='design' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
-
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="design"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
             {/* route for filter panel */}
             {filterProduct.panel.map((val, i) => {
-              return (<Route path={`/product/filter`} element={<FilterProduct referent='panel' chimuc={val} dataval={ProductList} filter={filter} data={dataProduct}    changeFilterData={changeFilterData} />} />)
+              return (
+                <Route
+                  path={`/product/filter`}
+                  element={
+                    <FilterProduct
+                      referent="panel"
+                      chimuc={val}
+                      dataval={ProductList}
+                      filter={filter}
+                      data={dataProduct}
+                      changeFilterData={changeFilterData}
+                    />
+                  }
+                />
+              );
             })}
-            <Route path='*' element={<>Error</>} />
+            <Route path="*" element={<>Error</>} />
             <Route path="/" element={<Home1 />} />
             <Route path="/compunentes/home/Home" element={<Home1></Home1>} />
             <Route
@@ -280,6 +458,6 @@ const App = props => {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
